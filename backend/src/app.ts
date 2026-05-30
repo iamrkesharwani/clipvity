6c837dcff47ebaa67f1e66d7f1e394fb2090c190
Rahul Kesharwani <132988447+iamrkesharwani@users.noreply.github.com>
2026-05-31 00:24:11 +0530

    add prod settings

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import compression from 'compression';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import helmet from 'helmet';
@@ -13,6 +14,8 @@ const corsOrigin = process.env['CORS_ORIGIN'] || 'http://localhost:5173';
 
 const app = express();
 
+app.set('trust proxy', 1);
+app.use(compression());
 app.use(helmet());
 app.use(cors({ origin: corsOrigin, credentials: true }));
 app.use(express.json());
